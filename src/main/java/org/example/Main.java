405aeec69757f2cd37efe25714ef58a186c18557
AugustLight <125802350+AugustLigh@users.noreply.github.com>
2024-05-17 23:24:48 +0300

    Try add Person class to repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,18 @@
 package org.example;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.example.dto.NameModel;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.google.gson.Gson;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        try (InputStream stream = Main.class.getClassLoader().getResourceAsStream("name.json")) {
-            Gson gson = new GsonBuilder()
-                    .setPrettyPrinting()
-                    .serializeNulls()
-                    .create();
+    final static Gson gson = new Gson();
 
-            NameModel dto = gson.fromJson(new String(stream.readAllBytes()), NameModel.class);
-            String toJson = gson.toJson(dto);
-            System.out.println(toJson);
-        }
+    public static void main(String[] args)  {
+        Person person = new Person();
+        person.setName("Dima");
+        person.setLastName("Tyasko");
+
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
